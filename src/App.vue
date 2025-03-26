2a5717a54472233738ad41327f1ed9574a34457c
eanlingyeung <129173561+eanlingyeung@users.noreply.github.com>
2025-03-26 15:13:28 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <div class="main">
         <div class="logo">
           <--<img src="https://q1.qlogo.cn/g?b=qq&nk=1655466387&s=640" />-->
-          <span>老宝贝今日热榜</span>
+          <span>老宝贝热榜</span>
         </div>
         <h2>提供各站热榜热搜聚合</h2>
       </div>
